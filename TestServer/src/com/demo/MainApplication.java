785af732e25c966c81c3a4d2a323bb74730a9a32
Thomas Schmidt <thomas.schmidt@stud.uni-regensburg.de>
2015-06-03 12:16:05 +0200

    Adding some oauth objects

--- TestServer/src/com/demo/MainApplication.java
+++ TestServer/src/com/demo/MainApplication.java
@@ -1,11 +1,17 @@
 package com.demo;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.oauth1.DefaultOAuth1Provider;
+import org.glassfish.jersey.server.oauth1.OAuth1ServerFeature;
 
 public class MainApplication extends ResourceConfig {
 	
 	public MainApplication(){
 		packages("com.demo");
+
+		DefaultOAuth1Provider provider = new DefaultOAuth1Provider();
+		OAuth1ServerFeature feature = new OAuth1ServerFeature(provider, "oauth/request", "oauth/access");
+		register(feature);
 	}
 
 }
