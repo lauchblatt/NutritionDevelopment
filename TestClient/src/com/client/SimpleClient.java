a558998073d25342e67f562ef06d5afca2355894
Thomas Schmidt <thomas.schmidt@stud.uni-regensburg.de>
2015-07-06 11:58:01 +0200

    Testing everything up to verifier

--- TestClient/src/com/client/SimpleClient.java
+++ TestClient/src/com/client/SimpleClient.java
@@ -5,16 +5,27 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 
+import org.glassfish.jersey.client.oauth1.AccessToken;
+import org.glassfish.jersey.client.oauth1.ConsumerCredentials;
+import org.glassfish.jersey.client.oauth1.OAuth1AuthorizationFlow;
+import org.glassfish.jersey.client.oauth1.OAuth1Builder;
+import org.glassfish.jersey.client.oauth1.OAuth1ClientSupport;
+
 public class SimpleClient {
 
 	public static void main(String[] args) {
 		
 		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://localhost:8080/TestServer/services/helloWorld/test");
-		Response response = target.request().get();
-		String responseData = response.readEntity(String.class);
-		System.out.println(responseData);
-
+		ConsumerCredentials consumerCredentials = new ConsumerCredentials("testKey", "testSecret");
+		OAuth1AuthorizationFlow authFlow = OAuth1ClientSupport.builder(consumerCredentials)
+			    .authorizationFlow(
+			        "http://localhost:8080/TestServer/services/oauth/request",
+			        "http://localhost:8080/TestServer/services/oauth/access",
+			        "http://localhost:8080/TestServer/services/oauth/redirection")
+			    .build();
+		
+		String authorizationUri = authFlow.start();
+		System.out.println("TEST");
 	}
 
 }
