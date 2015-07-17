92e1c5a26158602815c244688e0343233cf1f094
sistbien <stefan.bienk@web.de>
2015-07-17 21:35:19 +0200

    copy and extend test server

---
+++ TestServerExtended/src/com/demo/MainApplication.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.oauth1.DefaultOAuth1Provider;
+import org.glassfish.jersey.server.oauth1.OAuth1ServerFeature;
+
+
+
+public class MainApplication extends ResourceConfig {
+	
+	public MainApplication(){
+		packages("com.demo");
+
+		DefaultOAuth1Provider provider = new DefaultOAuth1Provider();
+		this.property("provider", provider);
+		OAuth1ServerFeature feature = new OAuth1ServerFeature(provider, "oauth/request", "oauth/access");
+		register(feature);
+	}
+
+}
