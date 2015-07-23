3760c2146976c21d1dd209f23662899cd055f35a
Thomas Schmidt <thomas.schmidt@stud.uni-regensburg.de>
2015-07-23 12:15:25 +0200

    testFeature

--- TestServer/src/com/demo/MainApplication.java
+++ TestServer/src/com/demo/MainApplication.java
@@ -1,5 +1,8 @@
 package com.demo;
 
+import javax.ws.rs.core.MultivaluedMap;
+
+import org.glassfish.jersey.internal.util.collection.MultivaluedStringMap;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.oauth1.DefaultOAuth1Provider;
 import org.glassfish.jersey.server.oauth1.OAuth1ServerFeature;
@@ -10,6 +13,8 @@ public class MainApplication extends ResourceConfig {
 		packages("com.demo");
 
 		DefaultOAuth1Provider provider = new DefaultOAuth1Provider();
+		MultivaluedMap<String, String> attributes = new MultivaluedStringMap();
+		provider.registerConsumer("id", "testKey", "testSecret", attributes);
 		OAuth1ServerFeature feature = new OAuth1ServerFeature(provider, "oauth/request", "oauth/access");
 		register(feature);
 	}
